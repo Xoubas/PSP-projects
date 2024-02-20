0470da7fcd00197a6116ce4eeb92638f0f036bdd
dam2 - Bayón Quintas <A21JavierBQ@sanclemente.local>
2024-02-20 14:32:24 +0100

    Emails

---
+++ SendEmails/src/main/java/org/example/SendEmail.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class SendEmail {
+    public static void main(String[] args) {
+        String sender = "gonzalocordeiromourelleoficial@gmail.com";
+        String receiver = "a22ivancp@iessanclemente.net";
+        Properties prop = null;
+        try (InputStream input = new FileInputStream("src/main/resources/smtp.properties")) {
+            prop = new Properties();
+            // load a properties file
+            prop.load(input);
+            // get the property value and print it out
+            System.out.println(prop.getProperty("mail.smtp.host"));
+            System.out.println(prop.getProperty("mail.smtp.port"));
+            System.out.println(prop.getProperty("mail.smtp.auth"));
+            System.out.println(prop.getProperty("mail.smtp.starttls.enalbe"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Authenticator auth = new Authenticator() {
+            //override the getPasswordAuthentication method
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(sender, "xsxq mvgr vfda eaca");
+            }
+        };
+        Session session = Session.getInstance(prop, auth);
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(sender));
+            message.setRecipient(Message.RecipientType.TO, new
+                    InternetAddress(receiver));
+            message.setSubject("Correo importante");
+            message.setText("La mano arriba cintura sola la media vuelta...");
+            int i = 100;
+            while(i>=0){
+                Transport.send(message);
+                System.out.println("Email sent.");
+                i--;
+            }
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            System.out.println("Error sending mail.");
+        }
+    }
+}
